393a7e11dda9d38f5a2baff1cdc20306637b84ea
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-12-05 01:57:10 +0300

    change log

--- cmd/auth-service/main.go
+++ cmd/auth-service/main.go
@@ -71,7 +71,7 @@ func setupLogger(env string) *slog.Logger {
 	case envDev:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	}
 
 	return log
